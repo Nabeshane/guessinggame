9479bfd5c7034c080074bcf63bfbedbb49527f18
Shane Gabel <gabels18@uww.edu>
2013-10-07 13:12:26 -0500

    Added multiple guesses

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -8,18 +8,20 @@ int  main () {
 	int guess;
 	srand (time(NULL));
 	int number = rand() % 100 + 1;
-	cout << "Please guess a number between one and one-hundred" << endl;
-	cin >> guess;
-	if (guess == number) {
-		cout << "You win!" << endl;
-	}
-	else if (guess < number) {
-		cout << "Too low!" << endl;
-	}
-	else if (guess > number) {
-		cout << "Too high!" << endl;
-	}
-	else {
-		cout << "you have not entered a number, you must enter a number to win." << endl;
-	}
+	do {
+		cout << "Please guess a number between one and one-hundred" << endl;
+		cin >> guess;
+		if (guess == number) {
+			cout << "You win!" << endl;
+		}
+		else if (guess < number) {
+			cout << "Too low!" << endl;
+		}
+		else if (guess > number) {
+			cout << "Too high!" << endl;
+		}
+		else {
+			cout << "you have not entered a number, you must enter a number to win." << endl;
+		}
+	}while (guess != number);
 }
