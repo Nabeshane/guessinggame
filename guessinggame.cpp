fe99e48e0fde0998552909fd906736faf0d22a1f
Shane Gabel <gabels18@uww.edu>
2013-10-07 13:18:25 -0500

    Prints out number of guesses

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 int  main () {
+	int numberofguesses = 0;
 	int guess;
 	srand (time(NULL));
 	int number = rand() % 100 + 1;
 	do {
+		numberofguesses++;
 		cout << "Please guess a number between one and one-hundred" << endl;
 		cin >> guess;
 		if (guess == number) {
@@ -24,4 +26,5 @@ int  main () {
 			cout << "you have not entered a number, you must enter a number to win." << endl;
 		}
 	}while (guess != number);
+	cout << numberofguesses << endl;
 }
